cfe75c86f99eb45a1b39a4a42725a6e8a92a2fdf
swapnib <swapnil.borpe@GMAIL.COM>
2019-10-26 16:52:55 +0530

    checking in boiler plate code

---
+++ src/main/java/com/tutorial/site/TutorialSiteApplication.java
@@ -0,0 +1,13 @@
+package com.tutorial.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialSiteApplication.class, args);
+	}
+
+}
